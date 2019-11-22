51e1a419ff0269b4fe5c4a7f6a6fdb95c4e2b849
Elias Verstappe <elias.verstappe@gmail.com>
2019-11-22 08:00:48 +0100

    make th login page work

--- documents/index.php
+++ documents/index.php
@@ -1,24 +1,44 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Lab Mechatronica</title>
-</head>
-<body>
+<?php
 
-<h1>Login page</h1>
+session_start();
 
-<form action="action_page.php" method="post">
-  
-  <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-    <button type="submit">Login</button>
-  </div>
-  <div class="container">
-    <span class="psw"><a href="#">Forgot password?</a></span>
-  </div>
-</form>
-</body>
-</html>
+if(!empty($_GET))
+{
+  $_SESSION["username"] = $_GET["username"];
+  $_SESSION["password"] = $_GET["password"];
+
+  header("Location: homepage.php");
+} else {
+  $dbc = mysqli_connect("localhost", "root", "", "lab_mechatronica")
+    or die (mysqli_connect_error($dbc));
+    echo (mysqli_connect_error($dbc));
+?>
+
+
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Lab Mechatronica</title>
+  </head>
+  <body>
+
+    <h1>Login page</h1>
+
+    <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="get">
+      
+      <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="username" required>
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
+        <button type="submit">Login</button>
+      </div>
+    </form>
+
+  </body>
+  </html>
+
+<?php
+}
+?>
+
