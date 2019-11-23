00dc85760cb36306f9da470d3ad5ca884b30d12a
Elias Verstappe <elias.verstappe@gmail.com>
2019-11-23 21:32:31 +0100

    adding new files

---
+++ documents/homepage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Lab Mechatronica</title>
+  </head>
+  <body>
+
+    <h1>Startpagina</h1>
+
+    Welkom op de startpagina.
+
+    <div class='navigationbar'>
+        <ul>
+            <li><a href="settings.php">Instellingen</a></li>
+            <li><a href="tracks.php">Trajecten</a></li>
+            <li><a href="libe_control.php">Live besturen</a></li>
+        </ul>
+    </div>
+
+
+
+
+  </body>
+  </html>
---
+++ documents/settings.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Lab Mechatronica</title>
+</head>
+<body>
+
+    <h1>Instellingen</h1>
+
+
+
+</body>
+</html>
