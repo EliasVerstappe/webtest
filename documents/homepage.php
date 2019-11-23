8a665798edea5a2cecade86280d7bcd069ac4fb9
Elias Verstappe <elias.verstappe@gmail.com>
2019-11-23 23:38:59 +0100

    editing homepage

--- documents/homepage.php
+++ documents/homepage.php
@@ -1,44 +1,44 @@
 <?php
-
-    session_start();
-    $_SESSION["logonsession"] = FALSE;
-
-    $dbc = mysqli_connect("localhost", "root", "", "lab_mechatronica")
-        or die (mysqli_connect_error($dbc));
-        echo (mysqli_connect_error($dbc));
-
-    if(empty($_SESSION["username"]) && empty($_SESSION["password"]))
-    {
-    echo "U bent niet ingelogd!";
-    echo "Klik <a href=\"index.php\"> Meld u opnieuw aan.</a>";
-    } else {
-        ?>
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit();
+}
+?>
 
 <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Lab Mechatronica</title>
-  </head>
-  <body>
+	<html>
+		<head>
+			<title>Lab Mechatronica</title>
+			<link href="../style.css" rel="stylesheet" type="text/css">
+			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+		</head>
+		
+		<body class="loggedin">
+	
+			<nav class="navtop">
+				<div>
+					<h1>SaLuJan - Robot</h1>
+					<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+					<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+				</div>
+			</nav>
 
-    <h1>Startpagina</h1>
+			<div class="content">
+				<h2>Home Page</h2>
 
-    Welkom op de startpagina.
+				<p>Welcome back, <?=$_SESSION['name']?>!</p>
 
-    <div class='navigationbar'>
-        <ul>
-            <li><a href="settings.php">Instellingen</a></li>
-            <li><a href="tracks.php">Trajecten</a></li>
-            <li><a href="libe_control.php">Live besturen</a></li>
-        </ul>
-    </div>
+				<div class='navigationbar'>
+					<ul>
+						<li><a href="settings.php">Instellingen</a></li>
+						<li><a href="tracks.php">Trajecten</a></li>
+						<li><a href="libe_control.php">Live besturen</a></li>
+					</ul>
+				</div>
+			</div>
+		</body>
+	</html>
 
-
-
-
-  </body>
-  </html>
-
-<?php
-    }
-?>
