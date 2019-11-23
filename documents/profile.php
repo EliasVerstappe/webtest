683e2aba5498d175b1d190b10c033c88bc4cc560
Elias Verstappe <elias.verstappe@gmail.com>
2019-11-23 23:48:18 +0100

    replace copy pasted table with the correct one and remove another email parameter

--- documents/profile.php
+++ documents/profile.php
@@ -15,7 +15,7 @@ if (mysqli_connect_errno()) {
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 // We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT password FROM accounts WHERE id = ?');
+$stmt = $con->prepare('SELECT password FROM users WHERE id = ?');
 // In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
@@ -52,10 +52,6 @@ $stmt->close();
 					<tr>
 						<td>Password:</td>
 						<td><?=$password?></td>
-					</tr>
-					<tr>
-						<td>Email:</td>
-						<td><?=$email?></td>
 					</tr>
 				</table>
 			</div>
