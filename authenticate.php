f673514d530cf980c79d3fa6dafa49eaa9c6b769
Elias Verstappe <elias.verstappe@gmail.com>
2019-11-23 23:39:32 +0100

    add line of code so that when credentials are correct you are navigated to the homepage

--- authenticate.php
+++ authenticate.php
@@ -40,7 +40,7 @@ if ($stmt->num_rows > 0) {
 		$_SESSION['loggedin'] = TRUE;
 		$_SESSION['name'] = $_POST['username'];
 		$_SESSION['id'] = $id;
-		echo 'Welcome ' . $_SESSION['name'] . '!';
+		header('Location: ./documents/homepage.php');
 	} else {
 		echo 'Incorrect password!';
 	}
