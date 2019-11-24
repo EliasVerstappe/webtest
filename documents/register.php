472dd4f7f143c1e6c7ff581d9139df5f0d48b287
Elias Verstappe <elias.verstappe@gmail.com>
2019-11-24 21:59:37 +0100

    worked on registration page

--- documents/register.php
+++ documents/register.php
@@ -1,5 +1,31 @@
 <?php
+    $msg = "";
+    
+    if (isset($_POST['submit'])) {
+        $dbc = mysqli_connect('localhost', 'root', '', 'lab_mechatronica');
+        if ( mysqli_connect_errno() ) {
+            die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+        }
 
+
+        if(!isset($_POST['name']) or !isset($_POST['email']) or !isset($_POST['password']) or !isset($_POST['cpassword']) ) {
+            $msg = "Fill in all the fields";
+        } else {
+            $name = ($_POST['name']);
+            $email = ($_POST['email']);
+            $password = ($_POST['password']);
+            $cpassword = ($_POST['cpassword']);
+            
+            if ($password != $cpassword) {
+                $msg = "Passwords don't match!";
+            } else {
+                $hashed_pwd = password_hash($password, PASSWORD_DEFAULT);
+                $dbc->query("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$hashed_pwd')");
+                $msg = "You have been registered succesfully!";
+                // Navigate to the page that says 'You have succesfully been registered'
+            }   
+        }
+    }
 ?>
 
 
@@ -15,31 +41,29 @@
 <body>
 
 <div class="login">
-  <h1>Register</h1>
+    <h1>Register</h1>
 
-  <form action="../resister.php" method="post">
+
+  <form action="register.php" method="post">
 
   
-      <label for="username">
-        <i class="fas fa-user"></i>
-      </label>
+      <label for="username"> <i class="fas fa-user"></i> </label>
       <input type="text" placeholder="Enter Username" name="username" id="username" required>
-      <label for="email">
-      <i class="fas fa-envelope"></i>
-      </label>
+      <label for="email"> <i class="fas fa-envelope"></i> </label>
       <input type="text" placeholder="Enter Email" name="email" id="email" required>
-      
 
-      <label for="password">
-        <i class="fas fa-lock"></i>
-      </label>
+      <label for="password"> <i class="fas fa-lock"></i> </label>
       <input type="password" placeholder="Enter Password" name="password" id="password" required>
-      <label for="password">
-        <i class="fas fa-lock"></i>
-      </label>
+      <label for="password"> <i class="fas fa-lock"></i> </label>
       <input type="password" placeholder="Confirm Password" name="cpassword" id="cpassword" required>
+    
+    <br><br>
+
+    <div>
+        <?php if($msg != "") echo $msg ;?>
+    </div>
       
-      <input type="submit" value="Sign up">
+        <input name="submit" type="submit" value="Sign up">
 
   </form>
 </div>
