829630da2c3d547d8986b29ee8081753f0faa281
Elias Verstappe <elias.verstappe@gmail.com>
2019-11-25 17:32:55 +0100

    adding succesful registration page

---
+++ documents/successful_registration.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration successful</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	<link href="../styles/successful_registration.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div class="registered">
+
+	<div>
+		<br>
+		<i class="fas fa-check"></i> You have successfully been registered!
+		<br><br>
+	</div>
+	
+	<input type="button" value="Sign in" class="sgninbtn">
+
+</div>
+
+</body>
+</html>
+
+
