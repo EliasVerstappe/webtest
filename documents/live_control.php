21255ea31438b4da4b0a269a7d473213886babe0
Elias Verstappe <elias.verstappe@gmail.com>
2019-12-05 18:20:37 +0100

    update the live control page, add gpio control

--- documents/live_control.php
+++ documents/live_control.php
@@ -9,48 +9,117 @@
 <!-- Scripts -->
     <script>
         function direction(wind){
+
+            <?php
+                system("gpio -g mode 10 out");
+                system("gpio -g mode 9 out");
+                system("gpio -g mode 11 out");
+                system("gpio -g mode 5 out");
+                system("gpio -g mode 6 out");
+                system("gpio -g mode 13 out");
+                system("gpio -g mode 19 out");
+                system("gpio -g mode 26 out");
+
+                system("gpio -g write 10 0");
+                system("gpio -g write 9 0");
+                system("gpio -g write 11 0");
+                system("gpio -g write 5 0");
+                system("gpio -g write 6 0");
+                system("gpio -g write 13 0");
+                system("gpio -g write 19 0");
+                system("gpio -g write 26 0");
+            ?>
+
             switch(wind) {
                 case northwest:
+                    <?php
+                        system("gpio -g write 6 1");
+                    ?>
                 
                     break;
                 
                 case north:
+                    <?php
+                        system("gpio -g write 26 1");
+                    ?>
                 
                     break;
                 
                 case northeast:
+                    <?php
+                        system("gpio -g write 19 1");
+                        system("gpio -g write 26 1");
+                    ?>
                 
                     break;
                 
                 case west:
+                    <?php
+                        system("gpio -g write 13 1");
+                        system("gpio -g write 19 1");
+                        system("gpio -g write 26 1");
+                    ?>
 
                     break;
                 
                 case east:
+                    <?php
+                        system("gpio -g write 19 1");
+                        system("gpio -g write 26 1");
+                    ?>
 
                     break;
 
                 case southwest:
+                    <?php
+                        system("gpio -g write 13 1");
+                        system("gpio -g write 19 1");
+                    ?>
 
                     break;
 
                 case south:
+                    <?php
+                        system("gpio -g write 13 1");
+                        system("gpio -g write 26 1");
+                    ?>
 
                     break;
 
                 case southeast:
+                    <?php
+                        system("gpio -g write 13 1");
+                    ?>
 
                     break;
 
                 case ccw:
+                    <?php
+                        system("gpio -g write 6 1");
+                        system("gpio -g write 19 1");
+                    ?>
 
                     break;
 
                 case cw:
+                    <?php
+                        system("gpio -g write 6 1");
+                        system("gpio -g write 26 1");
+                    ?>
 
                     break;
 
                 case stop:
+                    <?php
+                        system("gpio -g write 10 0");
+                        system("gpio -g write 9 0");
+                        system("gpio -g write 11 0");
+                        system("gpio -g write 5 0");
+                        system("gpio -g write 6 0");
+                        system("gpio -g write 13 0");
+                        system("gpio -g write 19 0");
+                        system("gpio -g write 26 0");
+                    ?>
 
                     break;
 
