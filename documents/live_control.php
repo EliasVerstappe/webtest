b6d1c17e1ade5dbc6e8a3f627ef1109b470de641
Elias Verstappe <elias.verstappe@gmail.com>
2019-12-05 16:58:31 +0100

    intermediate save

--- documents/live_control.php
+++ documents/live_control.php
@@ -6,6 +6,63 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
     </head>
 
+<!-- Scripts -->
+    <script>
+        function direction(wind){
+            switch(wind) {
+                case northwest:
+                
+                    break;
+                
+                case north:
+                
+                    break;
+                
+                case northeast:
+                
+                    break;
+                
+                case west:
+
+                    break;
+                
+                case east:
+
+                    break;
+
+                case southwest:
+
+                    break;
+
+                case south:
+
+                    break;
+
+                case southeast:
+
+                    break;
+
+                case ccw:
+
+                    break;
+
+                case cw:
+
+                    break;
+
+                case stop:
+
+                    break;
+
+
+                default:
+                    // zelfde code als STOP
+            }
+        }
+
+
+    </script>
+<!--  -->
     <body>
         <nav class="navtop">
             <div>
@@ -16,32 +73,38 @@
             </div>
         </nav>
 
-        <div class="buttontable">
+        <div class="contered-div">
         
-            <table>
+            
+
+            <table class="buttontable">
             <tr>
-                <td><i class="fas fa-expand-arrows-alt"></i></td>
-                <td><i class="fas fa-arrow-up"></i></td>
-                <td><i class="fas fa-expand-arrows-alt"></i></td>
+                <td><button onmousedown="direction('northwest')" onmouseup="direction('stop')"><i class="fas fa-expand-arrows-alt"></i></button></td>
+                <td><button onmousedown="direction('north')" onmouseup="direction('stop')"><i class="fas fa-arrow-up"></i></button></td>
+                <td><button onmousedown="direction('northeast')" onmouseup="direction('stop')"><i class="fas fa-expand-arrows-alt"></i></button></td>
             </tr>
             <tr>
-                <td><i class="fas fa-arrow-left"></i></td>
+                <td><button onmousedown="direction('west')" onmouseup="direction('stop')"><i class="fas fa-arrow-left"></i></button></td>
                 <td></td>
-                <td><i class="fas fa-arrow-right"></i></td>
+                <td><button onmousedown="direction('east')" onmouseup="direction('stop')"><i class="fas fa-arrow-right"></i></button></td>
             </tr>
             <tr>
-                <td><i class="fas fa-expand-arrows-alt"></i></td>
-                <td><i class="fas fa-arrow-down"></i></td>
-                <td><i class="fas fa-expand-arrows-alt"></i></td>
+                <td><button onmousedown="direction('southwest')" onmouseup="direction('stop')"><i class="fas fa-expand-arrows-alt"></i></button></td>
+                <td><button onmousedown="direction('south')" onmouseup="direction('stop')"><i class="fas fa-arrow-down"></i></button></td>
+                <td><button onmousedown="direction('southeast')" onmouseup="direction('stop')"><i class="fas fa-expand-arrows-alt"></i></button></td>
             </tr>
             <tr>
-                <td><i class="fas fa-undo-alt"></i></td>
+                <td><button onmousedown="direction('ccw')" onmouseup="direction('stop')"><i class="fas fa-undo-alt"></i></button></td>
                 <td></td>
-                <td><i class="fas fa-redo-alt"></i></td>
+                <td><button onmousedown="direction('cw')" onmouseup="direction('stop')"><i class="fas fa-redo-alt"></i></button></td>
             </tr>
             </table>
 
+
+
         </div>
+
+        
 
     </body>
 
