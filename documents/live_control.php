68943eed86755bd6697f912f19075fd02de8a3c5
Elias Verstappe <elias.verstappe@gmail.com>
2019-12-05 15:32:29 +0100

    adding the start of the live control page

---
+++ documents/live_control.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Live control</title>
+        <link href="../styles/live_control.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    </head>
+
+    <body>
+        <nav class="navtop">
+            <div>
+                <h1>Project :: Robot</h1>
+                <a href="homepage.php"><i class="fas fa-home"></i>Homepage</a>
+                <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+                <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+            </div>
+        </nav>
+
+        <div class="buttontable">
+        
+            <table>
+            <tr>
+                <td><i class="fas fa-expand-arrows-alt"></i></td>
+                <td><i class="fas fa-arrow-up"></i></td>
+                <td><i class="fas fa-expand-arrows-alt"></i></td>
+            </tr>
+            <tr>
+                <td><i class="fas fa-arrow-left"></i></td>
+                <td></td>
+                <td><i class="fas fa-arrow-right"></i></td>
+            </tr>
+            <tr>
+                <td><i class="fas fa-expand-arrows-alt"></i></td>
+                <td><i class="fas fa-arrow-down"></i></td>
+                <td><i class="fas fa-expand-arrows-alt"></i></td>
+            </tr>
+            <tr>
+                <td><i class="fas fa-undo-alt"></i></td>
+                <td></td>
+                <td><i class="fas fa-redo-alt"></i></td>
+            </tr>
+            </table>
+
+        </div>
+
+    </body>
+
+</html>
