b0857de5ba32215349878f6f7b97c938e8d1a6e1
Elias Verstappe <elias.verstappe@gmail.com>
2019-12-10 17:22:50 +0100

    clean up code

--- documents/gpio.php
+++ documents/gpio.php
@@ -7,12 +7,6 @@ if (isset ( $_GET["wind"] )) {
 	
 	//test if value is a string
 	if ( is_string($wind) ) {
-		
-		//set the gpio's mode to output		
-		system("gpio mode ".$wind." out");
-		//reading pin's status
-		exec ("gpio read ".$wind, $status, $return );
-		//set the gpio to high/low
         
         system("gpio -g mode 10 out");
         system("gpio -g mode 9 out");
@@ -33,89 +27,76 @@ if (isset ( $_GET["wind"] )) {
     
         switch($wind) {
             case 'northwest':
-                    system("gpio -g write 6 1");
-                
-            
+                system("gpio -g write 6 1");
                 break;
             
             case 'north':
-                
-                    system("gpio -g write 26 1");
-                
-            
+                system("gpio -g write 26 1");    
                 break;
             
             case 'northeast':
-                
-                    system("gpio -g write 19 1");
-                    system("gpio -g write 26 1");
-                
-            
+                system("gpio -g write 19 1");
+                system("gpio -g write 26 1");
                 break;
             
             case 'west':
-                
-                    system("gpio -g write 13 1");
-                    system("gpio -g write 19 1");
-                    system("gpio -g write 26 1");
-                
+                system("gpio -g write 13 1");
+                system("gpio -g write 19 1");
+                system("gpio -g write 26 1");
                 break;
             
             case 'east':
-                
-                    system("gpio -g write 19 1");
-                    system("gpio -g write 26 1");
-                
+                system("gpio -g write 19 1");
+                system("gpio -g write 26 1");
                 break;
+
             case 'southwest':
-                
-                    system("gpio -g write 13 1");
-                    system("gpio -g write 19 1");
-                
+                system("gpio -g write 13 1");
+                system("gpio -g write 19 1");
                 break;
+
             case 'south':
-                
-                    system("gpio -g write 13 1");
-                    system("gpio -g write 26 1");
-                
+                system("gpio -g write 13 1");
+                system("gpio -g write 26 1");
                 break;
+
             case 'southeast':
-                
-                    system("gpio -g write 13 1");
-                
+                system("gpio -g write 13 1");
                 break;
+
             case 'ccw':
-                
-                    system("gpio -g write 6 1");
-                    system("gpio -g write 19 1");
-                
+                system("gpio -g write 6 1");
+                system("gpio -g write 19 1");
                 break;
+
             case 'cw':
-                
-                    system("gpio -g write 6 1");
-                    system("gpio -g write 26 1");
-                
+                system("gpio -g write 6 1");
+                system("gpio -g write 26 1");
                 break;
+
             case 'stop':
-                    system("gpio -g write 10 0");
-                    system("gpio -g write 9 0");
-                    system("gpio -g write 11 0");
-                    system("gpio -g write 5 0");
-                    system("gpio -g write 6 0");
-                    system("gpio -g write 13 0");
-                    system("gpio -g write 19 0");
-                    system("gpio -g write 26 0");
-                
+                system("gpio -g write 10 0");
+                system("gpio -g write 9 0");
+                system("gpio -g write 11 0");
+                system("gpio -g write 5 0");
+                system("gpio -g write 6 0");
+                system("gpio -g write 13 0");
+                system("gpio -g write 19 0");
+                system("gpio -g write 26 0");
                 break;
+
             default:
-                // zelfde code als STOP
+                system("gpio -g write 10 0");
+                system("gpio -g write 9 0");
+                system("gpio -g write 11 0");
+                system("gpio -g write 5 0");
+                system("gpio -g write 6 0");
+                system("gpio -g write 13 0");
+                system("gpio -g write 19 0");
+                system("gpio -g write 26 0");
+                
         }
     });
-
-		//reading pin's status
-		exec ("gpio read ".$wind, $status, $return );
-		//print it to the client on the response
-		echo($status[0]);
 		
 	}
 	else { echo ("fail"); }
--- documents/script.js
+++ documents/script.js
@@ -1,15 +1,15 @@
 //TheFreeElectron 2015, http://www.instructables.com/member/TheFreeElectron/
-var button_0 = document.getElementById("button_0");
-var button_1 = document.getElementById("button_1");
-var button_2 = document.getElementById("button_2");
-var button_3 = document.getElementById("button_3");
-var button_4 = document.getElementById("button_4");
-var button_5 = document.getElementById("button_5");
-var button_6 = document.getElementById("button_6");
-var button_7 = document.getElementById("button_7");
+// var button_0 = document.getElementById("button_0");
+// var button_1 = document.getElementById("button_1");
+// var button_2 = document.getElementById("button_2");
+// var button_3 = document.getElementById("button_3");
+// var button_4 = document.getElementById("button_4");
+// var button_5 = document.getElementById("button_5");
+// var button_6 = document.getElementById("button_6");
+// var button_7 = document.getElementById("button_7");
 
 //Create an array for easy access later
-var Buttons = [ button_0, button_1, button_2, button_3, button_4, button_5, button_6, button_7];
+// var Buttons = [ button_0, button_1, button_2, button_3, button_4, button_5, button_6, button_7];
 
 //This function is asking for gpio.php, receiving datas and updating the index.php windtures
 function change_pin ( wind ) {
