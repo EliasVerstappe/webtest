b355a4cb219f8badd7c82d164a8434ac8f02f3e8
Daan Ver Eecke <daan.vereecke.dv@gmail.com>
2019-12-12 11:44:41 +0100

    added basic CSS to settings page.

--- documents/settings.php
+++ documents/settings.php
@@ -1,13 +1,20 @@
-<!DOCTYPE html>
 <html>
-<head>
-<title>Lab Mechatronica</title>
-</head>
-<body>
-
-    <h1>Instellingen</h1>
-
-
-
-</body>
+    <head>
+        <title>Lab Mechatronica</title>
+        <link href="../style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    </head>
+    <body>
+        <nav class="navtop">
+            <div>
+                <h1>SaLuJan - Robot</h1>
+                <a href="homepage.php"><i class="fas fa-home"></i>Homepage</a>
+                <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+                <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+            </div>
+        </nav>
+        <div class="content">
+            <h2>Settings Page</h2>
+        </div>    
+    </body>
 </html>
