27e5e7d94ecbd400f8e2a6174aa7aadde1649540
Elias Verstappe <elias.verstappe@gmail.com>
2019-12-12 18:17:19 +0100

    making progress on gpio

--- documents/gpio.php
+++ documents/gpio.php
@@ -4,106 +4,107 @@
 //Getting and using values
 if (isset ( $_GET["wind"] )) {
 
-    <script type="text/javascript">
-        console.log('got into gpio page');
-    </script>
+    echo "<script language='text/javascript'> alert('Just wanted to say $wind!'); </script>";
 
-	$wind = strip_tags ($_GET["wind"]);
-	
-	//test if value is a string
-	if ( is_string($wind) ) {
-        
-        system("gpio -g mode 10 out");
-        system("gpio -g mode 9 out");
-        system("gpio -g mode 11 out");
-        system("gpio -g mode 5 out");
-        system("gpio -g mode 6 out");
-        system("gpio -g mode 13 out");
-        system("gpio -g mode 19 out");
-        system("gpio -g mode 26 out");
-        system("gpio -g write 10 0");
-        system("gpio -g write 9 0");
-        system("gpio -g write 11 0");
-        system("gpio -g write 5 0");
-        system("gpio -g write 6 0");
-        system("gpio -g write 13 0");
-        system("gpio -g write 19 0");
-        system("gpio -g write 26 0");
+
+    // $wind = strip_tags ($_GET["wind"]);
     
-        switch($wind) {
-            case 'northwest':
-                system("gpio -g write 6 1");
-                break;
+    // $wind = 'north';
+	
+	// //test if value is a string
+	// if ( is_string($wind) ) {
+        
+    //     system("gpio -g mode 10 out");
+    //     system("gpio -g mode 9 out");
+    //     system("gpio -g mode 11 out");
+    //     system("gpio -g mode 5 out");
+    //     system("gpio -g mode 6 out");
+    //     system("gpio -g mode 13 out");
+    //     system("gpio -g mode 19 out");
+    //     system("gpio -g mode 26 out");
+    //     system("gpio -g write 10 0");
+    //     system("gpio -g write 9 0");
+    //     system("gpio -g write 11 0");
+    //     system("gpio -g write 5 0");
+    //     system("gpio -g write 6 0");
+    //     system("gpio -g write 13 0");
+    //     system("gpio -g write 19 0");
+    //     system("gpio -g write 26 0");
+    
+    //     switch($wind) {
+    //         case 'northwest':
+    //             system("gpio -g write 6 1");
+    //             break;
             
-            case 'north':
-                system("gpio -g write 26 1");    
-                break;
+    //         case 'north':
+    //             system("gpio -g write 26 1");    
+    //             break;
             
-            case 'northeast':
-                system("gpio -g write 19 1");
-                system("gpio -g write 26 1");
-                break;
+    //         case 'northeast':
+    //             system("gpio -g write 19 1");
+    //             system("gpio -g write 26 1");
+    //             break;
             
-            case 'west':
-                system("gpio -g write 13 1");
-                system("gpio -g write 19 1");
-                system("gpio -g write 26 1");
-                break;
+    //         case 'west':
+    //             system("gpio -g write 13 1");
+    //             system("gpio -g write 19 1");
+    //             system("gpio -g write 26 1");
+    //             break;
             
-            case 'east':
-                system("gpio -g write 19 1");
-                system("gpio -g write 26 1");
-                break;
+    //         case 'east':
+    //             system("gpio -g write 19 1");
+    //             system("gpio -g write 26 1");
+    //             break;
 
-            case 'southwest':
-                system("gpio -g write 13 1");
-                system("gpio -g write 19 1");
-                break;
+    //         case 'southwest':
+    //             system("gpio -g write 13 1");
+    //             system("gpio -g write 19 1");
+    //             break;
 
-            case 'south':
-                system("gpio -g write 13 1");
-                system("gpio -g write 26 1");
-                break;
+    //         case 'south':
+    //             system("gpio -g write 13 1");
+    //             system("gpio -g write 26 1");
+    //             break;
 
-            case 'southeast':
-                system("gpio -g write 13 1");
-                break;
+    //         case 'southeast':
+    //             system("gpio -g write 13 1");
+    //             break;
 
-            case 'ccw':
-                system("gpio -g write 6 1");
-                system("gpio -g write 19 1");
-                break;
+    //         case 'ccw':
+    //             system("gpio -g write 6 1");
+    //             system("gpio -g write 19 1");
+    //             break;
 
-            case 'cw':
-                system("gpio -g write 6 1");
-                system("gpio -g write 26 1");
-                break;
+    //         case 'cw':
+    //             system("gpio -g write 6 1");
+    //             system("gpio -g write 26 1");
+    //             break;
 
-            case 'stop':
-                system("gpio -g write 10 0");
-                system("gpio -g write 9 0");
-                system("gpio -g write 11 0");
-                system("gpio -g write 5 0");
-                system("gpio -g write 6 0");
-                system("gpio -g write 13 0");
-                system("gpio -g write 19 0");
-                system("gpio -g write 26 0");
-                break;
+    //         case 'stop':
+    //             system("gpio -g write 10 0");
+    //             system("gpio -g write 9 0");
+    //             system("gpio -g write 11 0");
+    //             system("gpio -g write 5 0");
+    //             system("gpio -g write 6 0");
+    //             system("gpio -g write 13 0");
+    //             system("gpio -g write 19 0");
+    //             system("gpio -g write 26 0");
+    //             break;
 
-            default:
-                system("gpio -g write 10 0");
-                system("gpio -g write 9 0");
-                system("gpio -g write 11 0");
-                system("gpio -g write 5 0");
-                system("gpio -g write 6 0");
-                system("gpio -g write 13 0");
-                system("gpio -g write 19 0");
-                system("gpio -g write 26 0");
-        }
-    });
+    //         default:
+    //             system("gpio -g write 10 0");
+    //             system("gpio -g write 9 0");
+    //             system("gpio -g write 11 0");
+    //             system("gpio -g write 5 0");
+    //             system("gpio -g write 6 0");
+    //             system("gpio -g write 13 0");
+    //             system("gpio -g write 19 0");
+    //             system("gpio -g write 26 0");
+    //     }
+    // });
 		
-	}
-	else { echo ("fail"); }
+	// }
+	// else { echo ("fail"); }
 } //print fail if cannot use values
 else { echo ("fail"); }
 ?>
--- documents/script.js
+++ documents/script.js
@@ -1,49 +1,38 @@
 //This function is asking for gpio.php, receiving datas
 function change_pin ( wind ) {
-var data = 0;
+// var data = 0;
 //send the wind string to gpio.php for changes
 //this is the http request
-	var request = new XMLHttpRequest();
-
-	// switch (wind) {
-	// 	case 'north':
-	// 		request.open( "GET" , "data0_on.php", true);
-	// 		break;
+	var north = "north";
+	window.location.href = ("gpio.php?wind=" + north);
 	
-	// 	default:
-	// 		request.open( "GET" , "data0_off.php", true);
-	// 		break;
-	// }
 
-
-	request.open( "GET" , "gpio.php?wind=" + wind, true);
-
-
-
-	request.send(null);
-	//receiving informations
-	request.onreadystatechange = function () {
-		if (request.readyState == 4 && request.status == 200) {
-			data = request.responseText;
-			if ( !(data.localeCompare("fail"))) {
-				alert ("Something went wrong! error fail" );
-				return ("fail");			
-			}
-			else {
-				alert ("Something went wrong! error kweet nie" );
-				return ("fail"); 
-			}
-		}
-		//test if fail
-		else if (request.readyState == 4 && request.status == 500) {
-			alert ("Failed to load resource: the server responded with a status of 500");
-			return ("fail");
-		}
-		//else 
-		else if (request.readyState == 4 && request.status != 200 && request.status != 500 ) { 
-			alert ("Something went wrong! error 4 en !200 en !500");
-			return ("fail"); }
-	}	
+// 	var request = new XMLHttpRequest();
+// 	request.open( "GET" , "gpio.php?wind=" + wind, true);
+// 	request.send(null);
+// 	// receiving informations
+// 	request.onreadystatechange = function () {
+// 		if (request.readyState == 4 && request.status == 200) {
+// 			data = request.responseText;
+// 			if ( !(data.localeCompare("fail"))) {
+// 				alert ("Something went wrong! error fail" );
+// 				return ("fail");			
+// 			}
+// 			else {
+// 				alert ("Something went wrong! error kweet nie" );
+// 				return ("fail"); 
+// 			}
+// 		}
+// 		//test if fail
+// 		else if (request.readyState == 4 && request.status == 500) {
+// 			alert ("Failed to load resource: the server responded with a status of 500");
+// 			return ("fail");
+// 		}
+// 		//else 
+// 		else if (request.readyState == 4 && request.status != 200 && request.status != 500 ) { 
+// 			alert ("Something went wrong! error 4 en !200 en !500");
+// 			return ("fail"); }
+// 	}	
 	
-return 0;
+// return 0;
 }
