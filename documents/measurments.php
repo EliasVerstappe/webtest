c21c8ec39205708efedac41d7a612b2e80cbbb3b
Daan Ver Eecke <daan.vereecke.dv@gmail.com>
2019-12-13 15:28:17 +0100

    tried to get the measure function refresh the table on press, not succesfull.

--- documents/measurments.php
+++ documents/measurments.php
@@ -30,7 +30,7 @@
             var Search;
 
             function MeasureFunc(){
-                "<?php addToDatabase() ?>";
+                <?php echo addToDatabase() ?>;
             }
 
             function Searchfunc(SearchOn){
@@ -98,21 +98,21 @@
                     <input type="text" id="SearchInput" value="Select Search criteria">
                     <button onclick="requestfunc()" type="button" id="SearchButton">Search</button>
                 </div>
-                <div class="result-content">
+                <div class="result-content" id="resulttable">
                     <?php
-                        $conn = mysqli_connect('localhost', 'root', '', 'lab_mechatronica');
-                        $sql = "SELECT number, distance, date, sensor from measurments order by number desc";
-                        $result = $conn->query($sql);
-                        if ($result->num_rows > 0) {
-                            echo "<table id=\"resulttable\"><tr><th>Numbers</th><th>Distance</th><th>Date</th><th>Sensor</th></tr>";
-                            while($row = $result->fetch_assoc()) {
-                                echo "<tr><td>" . $row["number"] . "</td><td>" . $row["distance"] .  "</td><td>" . $row["date"] . "</td><td>" . $row["sensor"] . "</td></tr>";
+                            $conn = mysqli_connect('localhost', 'root', '', 'lab_mechatronica');
+                            $sql = "SELECT number, distance, date, sensor from measurments order by number desc";
+                            $result = $conn->query($sql);
+                            if ($result->num_rows > 0) {
+                                echo "<table id=\"resulttable\"><tr><th>Numbers</th><th>Distance</th><th>Date</th><th>Sensor</th></tr>";
+                                while($row = $result->fetch_assoc()) {
+                                    echo "<tr><td>" . $row["number"] . "</td><td>" . $row["distance"] .  "</td><td>" . $row["date"] . "</td><td>" . $row["sensor"] . "</td></tr>";
+                                }
+                                echo "</table>";
+                            } else {
+                                echo "0 results";
                             }
-                            echo "</table>";
-                        } else {
-                            echo "0 results";
-                        }
-                        $conn->close();
+                            $conn->close();   
                     ?>
                 </div>
             </div>
