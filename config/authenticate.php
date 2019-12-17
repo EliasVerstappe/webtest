b9abab2149087c08e194f7377ec1c36a449ac7c0
Elias Verstappe <elias.verstappe@gmail.com>
2019-12-17 18:18:05 +0100

    use included dbc file in stead of separate connection

--- config/authenticate.php
+++ config/authenticate.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
-// Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'lab_mechatronica';
+
+include_once ('../config/db-conn.php');
+
 // Try and connect using the info above.
 $dbc = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
