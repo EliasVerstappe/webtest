e6033c1bef0ba2de799a4b8776d4317d1ed8abb6
Elias Verstappe <elias.verstappe@gmail.com>
2019-12-30 12:14:13 +0100

    removed the wrong lines, should be fixed now

--- documents/responses/user-handler.php
+++ documents/responses/user-handler.php
@@ -4,15 +4,17 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 $id = $data['data'];
 
+
 include_once ('../../config/db-conn.php');
 
+// $sql = "SELECT * FROM pending_users WHERE id=" . $id .";";
 $sql = "INSERT INTO users (username, email, password) 
 SELECT username, email, password 
 FROM pending_users
 WHERE id=" . $id .";";
+$result = $dbc->query($sql);
 
 $sql = "DELETE
 FROM pending_users
 WHERE id=" . $id .";";
-
-var_dump($result);
+$result = $dbc->query($sql);
